9572cc0591063ceb835537ba565c744bd97e34db
DawoudWork <dawoudwork@dawouds-MacBook-Pro.local>
2023-05-21 22:19:04 +0100

    Init

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { logo } from "@src/assets";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>hi</h1>
+    </div>
+  );
+};
+
+export default App;
